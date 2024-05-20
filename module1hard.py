3206bd8d8751444ce6842d774f33e2e28cd54f6c
Юлия <118264321+Morozova91@users.noreply.github.com>
2024-05-20 12:37:12 +0300

    module1hard.py

--- module1hard.py
+++ module1hard.py
@@ -14,6 +14,7 @@ two = sum(grades[1]) / len(grades[1])
 three = sum(grades[2]) / len(grades[2])
 four = sum(grades[3]) / len(grades[3])
 five = sum(grades[4]) / len(grades[4])
+dict = {}
 dict = {my_set[0]: one, my_set[1] : two,  my_set[2] : three,  my_set[3] : four,  my_set[4] : five}
 print(dict)
 
